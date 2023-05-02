ae364c1b9f79a48aff645f32c9ee9e279a247d6f
nessarz93 <126210620+nessarz93@users.noreply.github.com>
2023-05-02 23:16:22 +0000

    Actualización

--- src/js/app.js
+++ src/js/app.js
@@ -34,8 +34,8 @@ function render(variables = {}) {
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${variables.name} ${variables.lastname}</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
           <ul class= "${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fa fa-github"></i></a></li>
@@ -60,15 +60,15 @@ window.onload = function() {
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
+    twitter: "Twitter",
     github: "alesanchezr",
-    linkedin: null,
-    instagram: null,
+    linkedin: "linkedin",
+    instagram: "instagram",
     name: "Your name",
-    lastname: "",
-    role: null,
-    country: null,
-    city: null
+    lastname: "Last Name",
+    role: "Web Developer",
+    country: "Your country",
+    city: "Your city"
   };
   render(window.variables); //render the card for the first time
 
